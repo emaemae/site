0ae04c09e13e35f157cf9d8d2b48be676b98d4bc
emaemae <116826949+emaemae@users.noreply.github.com>
2022-11-11 12:45:20 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -2,18 +2,11 @@ const nav = document.querySelector('#nav');
 const navBtn = document.querySelector('#nav-btn');
 const navBtnImg = document.querySelector('#nav-btn-img');
 
-
-navBtn.addEventListener('click', function(){
-    open_func();
-});
-
-function open_func() {
-    navBtn.onclick = () => {
-        if (nav.classList.toggle('open')) {
-            navBtnImg.src = "img/close.png";
-        } else {
-            navBtnImg.src = 'img/burger.png';
-        }
+navBtn.onclick = () => {
+    if (nav.classList.toggle('open')) {
+        navBtnImg.src = "img/close.png";
+    } else {
+        navBtnImg.src = 'img/burger.png';
     }
 }
 
