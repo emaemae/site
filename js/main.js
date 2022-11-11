45c174067fb599154f5423101978d28d50b10597
emaemae <116826949+emaemae@users.noreply.github.com>
2022-11-11 12:50:08 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ const nav = document.querySelector('#nav');
 const navBtn = document.querySelector('#nav-btn');
 const navBtnImg = document.querySelector('#nav-btn-img');
 
-navBtn.onclick = () => {
+navBtn.ontouched = () => {
     if (nav.classList.toggle('open')) {
         navBtnImg.src = "img/close.png";
     } else {
